b41374289b2606601db65e48cf535454ca2b394c
aaryansinha15 <112536304+aaryansinha15@users.noreply.github.com>
2025-01-03 00:32:43 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Project based on Personal Expense Tracker. 
-
+from views.menu import display_menu
 def main():
     while True:
-        
+        display_menu()
+if __name__=="__main__":
+    main()
